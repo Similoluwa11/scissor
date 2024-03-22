86e0ee50dbb1626e525729320b5a9b7f9ff34c40
Similoluwa Akhigbe <akhigbesimiloluwa@gmail.com>
2024-03-22 09:15:35 +0100

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0MDg4Ng/spectral.js?branch_slug=master&token=569bb8a7-f3be-4004-9698-b3cb0cc1f24b";
+export default { extends: ruleset };
